6a5ce9aaf60a889aa82ec0a3cac2695e5f370512
theTurtleHermit <nicholas.ferry10@gmail.com>
2026-04-05 22:07:52 -0400

    Refactor code structure for improved readability and maintainability

--- projects/dagster-tutorial/src/dagster_tutorial/defs/assets.py
+++ projects/dagster-tutorial/src/dagster_tutorial/defs/assets.py
@@ -1,4 +1,6 @@
 import dagster as dg
+from dagster_duckdb import DuckDBResource
+from pathlib import Path
 
 
 @dg.asset
@@ -14,3 +16,18 @@ def orders() -> str:
 @dg.asset
 def payments() -> str:
     return "https://raw.githubusercontent.com/dbt-labs/jaffle-shop-classic/refs/heads/main/seeds/raw_payments.csv"
+
+
+@dg.asset
+def florida_real_estate(duckdb: DuckDBResource):
+    path: str = str(Path(__file__).parent.parent / "data" / "florida_real_estate_sold_properties_ultimate.csv")
+    table_name: str = "florida_real_estate"
+
+    with duckdb.get_connection() as conn:
+        conn.execute(
+            f"""
+            CREATE OR REPLACE TABLE {table_name} AS
+            SELECT *
+            FROM read_csv_auto('{path}')
+            """
+        )
